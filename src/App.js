4d990053ea368b0ee24c28aac6fcf9dbfba664f1
Werther <werther.dong@clearlabs.com>
2023-09-09 16:07:45 -0700

    checkin useless app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [fact, setFact] = useState("");
+
+  const generateFact = () => {
+    // Replace this with your own list of useless facts
+    const facts = [
+      "A cat has 32 muscles in each ear.",
+      "The average person spends 6 months of their lifetime waiting for a red light to turn green.",
+      "A sneeze travels out of your mouth at over 100 miles per hour.",
+      "The average person walks the equivalent of three times around the world in a lifetime.",
+      "A single cloud can weigh more than 1 million pounds.",
+    ];
+
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    setFact(randomFact);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Useless Facts</h1>
+      <p>{fact}</p>
+      <button onClick={generateFact}>Generate Fact</button>
     </div>
   );
-}
+};
 
 export default App;
